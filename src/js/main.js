880381268708f5a5eaf6c0b55504a8ece237038a
Lisa Bromark <libr2405@student.miun.se>
2025-03-03 13:06:09 +0100

    Mini-textfix

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,6 @@
 //Vid nästa klick ska animeringen avslutas. Hittade tips via följande hemsidor:
 // 1. https://www.youtube.com/watch?v=-Hhyhgp7n7o "Button Animation with HTML CSS JS | CSS Keyframe Animation"
 // 2. https://medium.com/@shivani.007/classlist-in-javascript-b73cfc598e70
-// 3. https://stackoverflow.com/questions/76157470/is-there-a-way-to-animate-a-button-on-click-with-classlist-add
 
 //För det första läget, innan knapptryckning, så används "remove" så att inget ska hända. När knapptryckning sedan sker
 //aktiveras läget där något händer genom "add" samt knappen ändrar text.
@@ -21,7 +20,7 @@ button.addEventListener("click", function(){
     }
 
     else{
-        
+
         dog.classList.add("animateDog");
         button.textContent="Hejdå Birdie!";
     }
